8bc0d66560da5f8f14c623e7f6e8b132c0370671
Holo <gfx.holo@proton.me>
2025-02-22 23:22:50 +1100

    feat: add numeric input logic

--- src/CalculiteView.ts
+++ src/CalculiteView.ts
@@ -83,37 +83,161 @@ export class CalculiteView extends ItemView {
 	}
 
 	/**
-	 * Create the buttons.
+	 * Create the buttons and register their listeners.
 	 */
 	private createButtons(): void {
 		// Create 1st row
-		this.contentEl.createEl('button', { cls: 'calculite-delete', text: CLEAR });
-		this.contentEl.createEl('button', { cls: 'calculite-delete', text: CLEAR_ENTRY });
-		this.contentEl.createEl('button', { cls: 'calculite-delete', text: BACK });
+		this.contentEl.createEl('button', { cls: 'calculite-delete', text: CLEAR }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressClear());
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-delete', text: CLEAR_ENTRY }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressClearEntry());
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-delete', text: BACK }, el => {
+			this.registerDomEvent(el, 'click', event => this.pressBack(event.shiftKey));
+		});
 		this.contentEl.createEl('button', { cls: 'calculite-operator', text: DIVIDE });
 
 		// Create 2nd row
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_7 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_8 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_9 });
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_7 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(7));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_8 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(8));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_9 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(9));
+		});
 		this.contentEl.createEl('button', { cls: 'calculite-operator', text: MULTIPLY });
 
 		// Create 3rd row
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_4 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_5 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_6 });
-		this.contentEl.createEl('button', { cls: 'calculite-operator', text: ADD });
-
-		// Create 4th row
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_1 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_2 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_3 });
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_4 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(4));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_5 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(5));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_6 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(6));
+		});
 		this.contentEl.createEl('button', { cls: 'calculite-operator', text: SUBTRACT });
 
+		// Create 4th row
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_1 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(1));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_2 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(2));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_3 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(3));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-operator', text: ADD });
+
 		// Create 5th row
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: NEGATE });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_0 });
-		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DECIMAL });
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: NEGATE }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressNegate());
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DIGIT_0 }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDigit(0));
+		});
+		this.contentEl.createEl('button', { cls: 'calculite-numeric', text: DECIMAL }, el => {
+			this.registerDomEvent(el, 'click', () => this.pressDecimal());
+		});
 		this.contentEl.createEl('button', { cls: 'calculite-operator', text: EQUALS });
+	}
+
+	/**
+	 * Clear the screen.
+	 */
+	private pressClear(): void {
+		this.currentInput = '0';
+
+		// Update screen
+		this.updateScreen(this.currentInput);
+	}
+
+	/**
+	 * Clear the screen.
+	 */
+	private pressClearEntry(): void {
+		this.currentInput = '0';
+
+		// Update screen
+		this.updateScreen(this.currentInput);
+	}
+
+	/**
+	 * Delete the last character from the screen.
+	 * @param shiftKey If true, delete the first character instead.
+	 */
+	private pressBack(shiftKey?: boolean): void {
+		// Remove one character from the input
+		this.currentInput = shiftKey
+			? this.currentInput.slice(1)
+			: this.currentInput.slice(0, -1);
+
+		// Clear input if no digits remains
+		if (this.currentInput.length === 0) {
+			this.currentInput = '0';
+		}
+
+		// Update screen
+		this.updateScreen(this.currentInput);
+	}
+
+	/**
+	 * Append a digit to the screen.
+	 * @param digit Number from 0-9.
+	 */
+	private pressDigit(digit: 0|1|2|3|4|5|6|7|8|9): void {
+		// If input is empty:
+		if (!this.currentInput) {
+			this.currentInput = String(digit);
+		// If input is not empty:
+		} else {
+			this.currentInput += digit;
+		}
+
+		// Update screen
+		this.updateScreen(this.currentInput);
+	}
+
+	/**
+	 * Append a decimal symbol to the screen.
+	 */
+	private pressDecimal(): void {
+		if (!this.currentInput.includes('.')) {
+			this.currentInput += '.';
+		}
+
+		// Update screen
+		this.updateScreen(this.currentInput);
+	}
+
+	/**
+	 * Toggle the input between positive & negative.
+	 */
+	private pressNegate(): void {
+		this.currentInput = this.currentInput.startsWith('-')
+			? this.currentInput.slice(1)
+			: '-' + this.currentInput;
+
+		// Update screen
+		this.updateScreen(this.currentInput);
+	}
+
+	/**
+	 * Update the screen.
+	 * @param output A number or string. Null clears the screen.
+	 */
+	private updateScreen(output: number | string | null): void {
+		output = String(output);
+
+		// Display correct minus symbol
+		output = output.replace('-', '−');
+
+		// Output to screen
+		this.screenEl.setText(output);
 	}
 }
